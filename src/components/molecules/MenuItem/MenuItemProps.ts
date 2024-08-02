60380054487dcb3ac6d2aa8e9fb68367f15ae8d0
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-08-02 17:04:19 +0500

    refactor: update MenuItemProps for improved type safety and flexibility

--- src/components/molecules/MenuItem/MenuItemProps.ts
+++ src/components/molecules/MenuItem/MenuItemProps.ts
@@ -1,6 +1,7 @@
 import { MenuItemIconProps } from '@/components/atoms/MenuItemIcon/MenuItemIconProps';
 
 export interface MenuItemProps {
+  id: number;
   href: '/' | '/about' | '/subscribe' | '/projects' | '/resume';
   icon: MenuItemIconProps['name'];
   text: 'Home' | 'About' | 'Subscribe' | 'Projects' | 'Resume';
