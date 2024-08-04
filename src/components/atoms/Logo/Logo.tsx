2240d579da11a364f82c704339f155a242987a1d
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-08-04 18:00:20 +0500

    refactor(Logo): replace Image component with StrapiImage component

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -2,23 +2,22 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import Image from 'next/image';
-import HeroImage from '@/assets/images/me.jpg';
+import { StrapiImage } from '@/components/atoms/StrapiImage/StrapiImage';
+import { LogoProps } from './LogoProps';
 
-export default function Logo() {
+export default function Logo({ src, alt }: LogoProps) {
   const pathname = usePathname();
   return (
     <>
       {pathname !== '/' && (
         <div className="flex justify-center my-4">
           <Link href="/">
-            <Image
-              className="rounded-full"
-              src={HeroImage}
+            <StrapiImage
+              src={src}
+              alt={alt}
               width={32}
               height={32}
-              priority
-              alt="Me"
+              className="object-cover rounded-full border-2 border-gray-300 hover:border-sky-500 transition-transform transform hover:scale-105 duration-300 shadow-lg"
             />
           </Link>
         </div>
